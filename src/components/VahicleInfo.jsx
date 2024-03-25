9baed6bac8f7bd5e5e59f6f271bc631f0180b7e5
Kavindu Sanjula <ravanadevs@gmail.com>
2024-03-25 13:35:08 +0530

    Create vehicle info

---
+++ src/components/VahicleInfo.jsx
@@ -0,0 +1,28 @@
+import RPMmeter from './RPMmeter'
+import Speedometer from './Speedometer'
+import './css/vehicleinfo.css'
+
+const VehileInfo = () => {
+  return (
+    <div className='vehicle-info-container'>
+      <div className='dials'>
+        <div className='dial speed'>
+          <Speedometer speed={60} />
+          <span>Speed</span>
+        </div>
+        <div className='dial rpm'>
+          <RPMmeter speed={2520} />
+          <span>RPM</span>
+        </div>
+      </div>
+      <div className='info'>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione
+        blanditiis quod porro! Officiis a quis dolorem minima doloremque,
+        officia molestiae asperiores adipisci libero ullam. Dolore nulla saepe
+        ratione temporibus facere.
+      </div>
+    </div>
+  )
+}
+
+export default VehileInfo
