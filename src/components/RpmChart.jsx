b270ebf35bbc80d52356cc6b721eda6320c5de88
Kavindu Sanjula <ravanadevs@gmail.com>
2024-05-16 23:12:45 +0530

    RPM Chart

--- src/components/RpmChart.jsx
+++ src/components/RpmChart.jsx
@@ -1,17 +1,40 @@
 import './css/rpm-chart.css'
 import { Line } from 'react-chartjs-2'
-import { CategoryScale, Chart, LineElement, LinearScale } from 'chart.js'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
 
-Chart.register(CategoryScale)
-Chart.register(LinearScale)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+  // Title,
+  // Tooltip,
+  // Legend
+)
+
 const RpmChart = () => {
-  const labels = [1, 2, 3, 4, 5, 6, 7]
+  const labels = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23,
+  ]
   const data = {
     labels: labels,
     datasets: [
       {
-        label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
+        label: 'RPM',
+        data: [
+          820, 853, 840, 880, 950, 990, 1059, 1123, 1852, 2530, 2780, 2960,
+          3152, 3200, 2520, 2150, 1560, 1000, 940, 853, 870, 920, 850,
+        ],
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1,
@@ -20,8 +43,9 @@ const RpmChart = () => {
   }
 
   return (
-    <div className='chart-container d-flex flex-column align-items-center'>
-      <div>
+    <div className='chart-container d-flex flex-column align-items-center p-5'>
+      <h4>RPM</h4>
+      <div className='w-100 h-100'>
         <Line data={data} />
       </div>
     </div>
